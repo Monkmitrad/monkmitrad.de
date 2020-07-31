942fdd9cb6e53e7336980c8f95a56df9e388ab89
monkmitrad <monkmitrad@gmail.com>
2020-07-31 22:14:49 +0200

    added legal notice and privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TitleComponent } from './title/title.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: TitleComponent },
+  { path: 'legal-notice', component: LegalNoticeComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
